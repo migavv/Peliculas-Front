19c70584255b41c036109be1f3fec10e2e50542f
Miguel Gaviria Escobar <migaviria@outlook.com>
2023-03-29 11:02:41 -0500

    Search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,28 @@
+import styles from "./Search.module.css";
+import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export function Search() {
+  const [searchText, setSearchText] = useState("");
+  const history = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history("/?search=" + searchText);
+  };
+  return (
+    <form className={styles.searchContainer} onSubmit={handleSubmit}>
+      <div className={styles.searchBox}>
+        <input
+          className={styles.searchInput}
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button className={styles.searchButton} type="submit">
+          <FaSearch size={20} />
+        </button>
+      </div>
+    </form>
+  );
+}
